ade98f88d8aa10d3446a08e8d49837460b1ce833
ritik-hedau18 <ritikhedau253@gmail.com>
2026-03-06 14:40:33 +0000

    feat: configure STOMP WebSocket server skeleton using SockJS

---
+++ audit-log-service/src/main/java/com/echo/auditlogservice/AuditLogServiceApplication.java
@@ -0,0 +1,13 @@
+package com.echo.auditlogservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class AuditLogServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AuditLogServiceApplication.class, args);
+    }
+}
---
+++ audit-log-service/src/main/java/com/echo/auditlogservice/config/KafkaConsumerConfig.java
@@ -0,0 +1,23 @@
+package com.echo.auditlogservice.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.listener.DeadLetterPublishingRecoverer;
+import org.springframework.kafka.listener.DefaultErrorHandler;
+import org.springframework.util.backoff.FixedBackOff;
+
+@Configuration
+@Slf4j
+public class KafkaConsumerConfig {
+
+    @Bean
+    public DefaultErrorHandler errorHandler(KafkaTemplate<Object, Object> template) {
+        log.info("Registering Kafka consumer DefaultErrorHandler with DeadLetterPublishingRecoverer in Audit Log Service");
+        return new DefaultErrorHandler(
+                new DeadLetterPublishingRecoverer(template),
+                new FixedBackOff(1000L, 2)
+        );
+    }
+}

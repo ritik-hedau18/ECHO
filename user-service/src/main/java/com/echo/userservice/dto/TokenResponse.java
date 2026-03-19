ff8d8d79e467b2f260e858732a07153d14a0ef2f
ritik-hedau18 <ritikhedau253@gmail.com>
2026-03-19 17:11:39 +0000

    fix: input box positioning offset during text drawing

---
+++ user-service/src/main/java/com/echo/userservice/dto/TokenResponse.java
@@ -0,0 +1,19 @@
+package com.echo.userservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class TokenResponse {
+    private String accessToken;
+    private String refreshToken;
+    private String userId;
+    private String username;
+    private String email;
+    private String avatarUrl;
+}
